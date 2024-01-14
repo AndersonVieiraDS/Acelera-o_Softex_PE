d2a68169ee04feb0e99350dddbf639da17c4fa1b
Your Name <anderson.mfvs@hotmail.com>
2024-01-14 15:13:20 -0300

    Commit

--- Typescript/Projeto Coworking/app.ts
+++ Typescript/Projeto Coworking/app.ts
@@ -40,6 +40,33 @@ app.get('/criar-tabela', async (req, res) => {
   }
 });
 
+// Rota para atualizar um usuário pelo ID
+app.put('/atualizar-usuario/:id', async (req, res) => {
+  const userId = req.params.id;
+  const { nome } = req.body;
+
+  try {
+    const result = await pool.query('UPDATE usuarios SET nome = $1 WHERE id = $2 RETURNING *', [nome, userId]);
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error('Erro ao atualizar o usuário:', error);
+    res.status(500).send('Erro interno do servidor ao atualizar o usuário');
+  }
+});
+
+// Rota para excluir um usuário pelo ID
+app.delete('/excluir-usuario/:id', async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const result = await pool.query('DELETE FROM usuarios WHERE id = $1 RETURNING *', [userId]);
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error('Erro ao excluir o usuário:', error);
+    res.status(500).send('Erro interno do servidor ao excluir o usuário');
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
