f8760f7a8181186208d05a8944b62c518f49f4a4
Anderson <anderson.mfvs@hotmail.com>
2024-06-11 10:20:02 -0300

    atualizado

--- src/db.ts
+++ src/db.ts
@@ -2,8 +2,7 @@ import knex from 'knex';
 import config  from "./knexfile"
 
 const environment = process.env.NODE_ENV || 'development';
-const connectionConfig = config[environment];
 
-const db = knex(connectionConfig);
+const db = knex(config[environment]);
 
 export default db;
